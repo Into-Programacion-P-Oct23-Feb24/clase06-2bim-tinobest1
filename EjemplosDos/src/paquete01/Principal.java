a34b603a2062d01f0b5c9873e8c6062a078c0437
TinoBest <sayayinlenin@hotmail.com>
2024-01-18 14:11:13 -0500

    -aempresa

--- EjemplosDos/src/paquete01/Principal.java
+++ EjemplosDos/src/paquete01/Principal.java
@@ -6,6 +6,7 @@
 package paquete01;
 
 import paquete02.DatoPersonal;
+import paquete02.DatoTrabajo;
 import paquete03.DatosUbicacion;
 import paquete04.DatoAcademico;
 import paquete05.DatoFinal;
@@ -20,26 +21,40 @@ public class Principal {
         String nombre;
         String ciudad;
         String apellidoRetornado;
-        
+        String acomulador = "";
+        String nombreEmpresa;
+        String direccion;
+
         double[] misNotas;
         double promedio;
-        
+
         nombre = DatoPersonal.obtenerNombre();
         apellidoRetornado = DatoPersonal.obtenerApellido();
+        nombreEmpresa = DatoTrabajo.obtenerEmpresa();
+        direccion = DatoTrabajo.obtenerDireccion();
         ciudad = DatosUbicacion.obtenerCiudad();
         misNotas = DatoAcademico.obtenerNotas(4);
         promedio = DatoFinal.obtenerPromedio(misNotas);
-        
-        
+
+        for (int i = 0; i < misNotas.length; i++) {
+            acomulador = String.format("%s\nNota%d: %.2f\n", acomulador,
+                    i + 1, misNotas[i]);
+        }
         System.out.printf("Los datos ingresados son:\n"
                 + "Nombre: %s\n"
                 + "Apellido: %s\n"
+                + "Nombre de la empresa: %s\n"
+                + "Direccion de la empresa: %s\n"
                 + "Ciudad: %s\n"
+                + "notas: %s\n"
                 + "Promedio: %.2f\n",
                 nombre,
                 apellidoRetornado,
+                nombreEmpresa,
+                direccion,
                 ciudad,
+                acomulador,
                 promedio);
+
     }
-    
 }
--- EjemplosDos/src/paquete02/DatoTrabajo.java
+++ EjemplosDos/src/paquete02/DatoTrabajo.java
@@ -5,12 +5,30 @@
  */
 package paquete02;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class DatoTrabajo {
     // método que permita preguntar al usuario el nombre de la empresa
+    public static String obtenerEmpresa(){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese nombre el nombre de la empresa");
+        String nombreEmpresa = entrada.nextLine();
+        
+        return nombreEmpresa;
+    }
+    
+    public static String obtenerDireccion(){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese Direccion de la empresa");
+        String Direccion = entrada.nextLine();
+        
+        return Direccion;
+    }
+}
     
     // método que permita preguntar al usuario la dirección de la empresa
-}
+
